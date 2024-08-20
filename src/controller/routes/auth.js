44ec38ebaaef514532fbafed28bd30140e3dd0a9
Cizerthapa <cizerthapa@gmail.com>
2024-08-20 14:38:05 +0545

    Login and register done

--- src/controller/routes/auth.js
+++ src/controller/routes/auth.js
@@ -8,9 +8,9 @@ const router = express.Router();
 
 router.post('/register', async (req, res) => {
     try {
-        const { username, password } = req.body;
+        const { name, email, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ username, password: hashedPassword });
+        const user = new User({ name, email, password: hashedPassword });
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
@@ -20,9 +20,9 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const { username, password } = req.body;
+        const { email, password } = req.body;
 
-        const user = await User.findOne({ username });
+        const user = await User.findOne({ email });
         if (!user) {
             return res.status(401).json({ error: 'User not found' });
         }
